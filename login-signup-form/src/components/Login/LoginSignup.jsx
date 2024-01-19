c4f25f50faf2a3fc31238eccfd3443d5a2f073a5
programmerNizam <mdnizam12109@gmail.com>
2024-01-20 03:06:54 +0600

    [Manage] action state and add conditional button

--- login-signup-form/src/components/Login/LoginSignup.jsx
+++ login-signup-form/src/components/Login/LoginSignup.jsx
@@ -1,12 +1,14 @@
+import { useState } from "react";
 import Email_Icon from "../../assets/email.png";
 import Password_Icon from "../../assets/password.png";
 import User_Icon from "../../assets/person.png";
 import "./LoginSignup.css";
 export default function LoginSignup() {
+  const [action, setAction] = useState("Sign Up");
   return (
     <div className="container">
       <div className="header">
-        <div className="text">Sing Up</div>
+        <div className="text">{action}</div>
         <div className="underline"></div>
       </div>
       <div className="inputs">
@@ -27,8 +29,18 @@ export default function LoginSignup() {
         Lost Password? <span>Click Here!</span>
       </div>
       <div className="submit-container">
-        <div className="submit">Sign Up</div>
-        <div className="submit">Login</div>
+        <div
+          className={action === "Login" ? "submit gray" : "submit"}
+          onClick={() => setAction("Sign Up")}
+        >
+          Sign Up
+        </div>
+        <div
+          className={action === "Sign Up" ? "submit gray" : "submit"}
+          onClick={() => setAction("Login")}
+        >
+          Login
+        </div>
       </div>
     </div>
   );
