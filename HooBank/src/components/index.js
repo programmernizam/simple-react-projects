0fc7161b78c4d1a9a00642ef7653d7eff2647f22
programmerNizam <mdnizam12109@gmail.com>
2024-01-21 21:46:59 +0600

    [Created] Constant Component import file

--- HooBank/src/components/index.js
+++ HooBank/src/components/index.js
@@ -1,5 +1,17 @@
+import Billing from "./Billing"
+import Business from "./Business"
+import CTA from "./CTA"
+import CardDeal from "./CardDeal"
+import Clients from "./Clients"
+import Footer from "./Footer"
+import Hero from "./Hero"
 import Navbar from "./Navbar"
+import Stats from "./Stats"
+import Testimonials from "./Testimonials"
 
 export {
-    Navbar
+    Billing,
+    Business, CTA, CardDeal,
+    Clients, Footer,
+    Hero, Navbar, Stats, Testimonials
 }
