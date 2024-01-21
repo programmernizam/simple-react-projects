eb6dc15c54638d396bcfbaf62ecf3d44a83a9345
programmerNizam <mdnizam12109@gmail.com>
2024-01-21 21:33:51 +0600

    [Created] Billing, Business & Button components

--- HooBank/src/App.jsx
+++ HooBank/src/App.jsx
@@ -10,6 +10,10 @@ export default function App() {
       <div className={`bg-primary ${styles.flexStart}`}>
         <div className={`${styles.boxWidth}`}>Hero</div>
       </div>
+      {/* Stats */}
+      <div className={`bg-primary ${styles.flexStart} ${styles.paddingX}`}>
+        <div className={`${styles.boxWidth}`}>Main</div>
+      </div>
     </div>
   );
 }
