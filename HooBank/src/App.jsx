84424dd44090d55d5a9e534690985a9e4a74c372
programmerNizam <mdnizam12109@gmail.com>
2024-01-21 21:30:24 +0600

    [Added] Navbar & Hero Section Boiler

--- HooBank/src/App.jsx
+++ HooBank/src/App.jsx
@@ -1,3 +1,15 @@
+import styles from "./style";
 export default function App() {
-  return <></>;
+  return (
+    <div className="bg-primary w-full overflow-hidden">
+      {/* Navbar */}
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>Navbar</div>
+      </div>
+      {/* Hero */}
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>Hero</div>
+      </div>
+    </div>
+  );
 }
