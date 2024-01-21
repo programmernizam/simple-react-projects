1d215dec59c74e2844123894b973e553da0490a0
programmerNizam <mdnizam12109@gmail.com>
2024-01-21 21:51:14 +0600

    [Added] All Component in app compoent

--- HooBank/src/App.jsx
+++ HooBank/src/App.jsx
@@ -1,18 +1,43 @@
+import {
+  Billing,
+  Business,
+  CTA,
+  CardDeal,
+  Clients,
+  Footer,
+  Hero,
+  Navbar,
+  Stats,
+  Testimonials,
+} from "./components";
 import styles from "./style";
 export default function App() {
   return (
     <div className="bg-primary w-full overflow-hidden">
       {/* Navbar */}
       <div className={`${styles.paddingX} ${styles.flexCenter}`}>
-        <div className={`${styles.boxWidth}`}>Navbar</div>
+        <div className={`${styles.boxWidth}`}>
+          <Navbar />
+        </div>
       </div>
       {/* Hero */}
       <div className={`bg-primary ${styles.flexStart}`}>
-        <div className={`${styles.boxWidth}`}>Hero</div>
+        <div className={`${styles.boxWidth}`}>
+          <Hero />
+        </div>
       </div>
       {/* Stats */}
       <div className={`bg-primary ${styles.flexStart} ${styles.paddingX}`}>
-        <div className={`${styles.boxWidth}`}>Main</div>
+        <div className={`${styles.boxWidth}`}>
+          <Stats />
+          <Business />
+          <Billing />
+          <CardDeal />
+          <Testimonials />
+          <Clients />
+          <CTA />
+          <Footer />
+        </div>
       </div>
     </div>
   );
