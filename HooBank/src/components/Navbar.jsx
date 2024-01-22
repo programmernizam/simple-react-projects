f472b1eaa3b9f27547d084c22001f3b51a9fd9ba
programmerNizam <mdnizam12109@gmail.com>
2024-01-22 15:27:36 +0600

    [Start] Building Navbar

--- HooBank/src/components/Navbar.jsx
+++ HooBank/src/components/Navbar.jsx
@@ -1,3 +1,12 @@
+import { close, logo, menu } from "../assets";
+import { navLinks } from "../constants";
 export default function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <div>
+      <img src={close} alt="" />
+      <img src={logo} alt="" />
+      <img src={menu} alt="" />
+      <a href={navLinks.home}></a>
+    </div>
+  );
 }
