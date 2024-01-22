fc5eaf59e8c7f9498efc0db9f4fd6d8b8224b32c
programmerNizam <mdnizam12109@gmail.com>
2024-01-22 21:51:41 +0600

    [Done] Desktop version of navbar

--- HooBank/src/components/Navbar.jsx
+++ HooBank/src/components/Navbar.jsx
@@ -1,12 +1,21 @@
-import { close, logo, menu } from "../assets";
+import { logo } from "../assets";
 import { navLinks } from "../constants";
 export default function Navbar() {
   return (
-    <div>
-      <img src={close} alt="" />
-      <img src={logo} alt="" />
-      <img src={menu} alt="" />
-      <a href={navLinks.home}></a>
-    </div>
+    <nav className="w-full flex py-6 justify-between items-center navbar">
+      <img src={logo} alt="hoobank" className="w-[124px] h-[32px]" />
+      <ul className="list-none sm:flex hidden justify-end items-center flex-1">
+        {navLinks.map((nav, index) => (
+          <li
+            key={nav.id}
+            className={`font-poppins font-normal cursor-pointer text-[16px] ${
+              index === navLinks.length - 1 ? "mr-0" : "mr-10"
+            } text-white`}
+          >
+            <a href={`#${nav.id}`}>{nav.title}</a>
+          </li>
+        ))}
+      </ul>
+    </nav>
   );
 }
