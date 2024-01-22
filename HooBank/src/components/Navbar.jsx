42ba7b44d6b5db1537cb56e36481acdb65bc45f0
programmerNizam <mdnizam12109@gmail.com>
2024-01-22 21:56:06 +0600

    [Done] Menu Toggle button

--- HooBank/src/components/Navbar.jsx
+++ HooBank/src/components/Navbar.jsx
@@ -1,9 +1,12 @@
-import { logo } from "../assets";
+import { useState } from "react";
+import { close, logo, menu } from "../assets";
 import { navLinks } from "../constants";
 export default function Navbar() {
+  const [toggle, setToggle] = useState(false);
   return (
     <nav className="w-full flex py-6 justify-between items-center navbar">
       <img src={logo} alt="hoobank" className="w-[124px] h-[32px]" />
+      {/* Desktop Version */}
       <ul className="list-none sm:flex hidden justify-end items-center flex-1">
         {navLinks.map((nav, index) => (
           <li
@@ -16,6 +19,15 @@ export default function Navbar() {
           </li>
         ))}
       </ul>
+      {/* Mobile Version */}
+      <div className="sm:hidden flex flex-1 justify-end items-center">
+        <img
+          src={toggle ? close : menu}
+          alt="menu"
+          className="w-[28px] h-[28px] object-contain"
+          onClick={() => setToggle((prev) => !prev)}
+        />
+      </div>
     </nav>
   );
 }
