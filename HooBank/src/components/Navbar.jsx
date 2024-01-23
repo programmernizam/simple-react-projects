8a5f014809fc002ec903363aed3cc4d3f50aaf79
programmerNizam <mdnizam12109@gmail.com>
2024-01-23 19:24:07 +0600

    [Added] Mobile Menu Styles

--- HooBank/src/components/Navbar.jsx
+++ HooBank/src/components/Navbar.jsx
@@ -27,6 +27,11 @@ export default function Navbar() {
           className="w-[28px] h-[28px] object-contain"
           onClick={() => setToggle((prev) => !prev)}
         />
+        <div
+          className={`${
+            toggle ? "flex" : "hidden"
+          } p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
+        ></div>
       </div>
     </nav>
   );
