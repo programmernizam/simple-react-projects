7ed2a0a192bc8c21c28346a50024a2ca843eaa80
programmerNizam <mdnizam12109@gmail.com>
2024-01-26 14:14:19 +0600

    [Start] Building Hero

--- HooBank/src/components/Hero.jsx
+++ HooBank/src/components/Hero.jsx
@@ -1,5 +1,11 @@
+import styles from "../style";
 export default function Hero() {
   return (
-    <div>Hero</div>
-  )
+    <section
+      className={`flex md:flex-row flex-col ${styles.paddingY}`}
+      id="home"
+    >
+      <div></div>
+    </section>
+  );
 }
