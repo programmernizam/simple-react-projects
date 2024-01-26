7137518790ee94606d8b47107ac468cfe518dd31
programmerNizam <mdnizam12109@gmail.com>
2024-01-26 14:57:10 +0600

    [Done] Get Started

--- HooBank/src/components/GetStarted.jsx
+++ HooBank/src/components/GetStarted.jsx
@@ -9,14 +9,14 @@ export default function GetStarted() {
         className={`${styles.flexCenter} flex-col bg-primary w-[100%] h-[100%] rounded-full`}
       >
         <div className={`${styles.flexStart} flex-row`}>
-          <p className="font-poppins font-medium text-[18px] leading-[23px]">
+          <p className="font-poppins font-medium text-[18px] leading-[23px] mr-2">
             <span className="text-gradient">Get</span>
-            <img
-              src={arrowUp}
-              alt="arrowUp"
-              className="w-[23px] h-[23px] object-contain"
-            />
           </p>
+          <img
+            src={arrowUp}
+            alt="arrowUp"
+            className="w-[23px] h-[23px] object-contain"
+          />
         </div>
         <div className={`${styles.flexStart} flex-row`}>
           <p className="font-poppins font-medium text-[18px] leading-[23px]">
