852863b327d87840d7d169e2d701364498bd1c01
programmerNizam <mdnizam12109@gmail.com>
2024-01-26 14:26:13 +0600

    [Added] Hero Heading Text

--- HooBank/src/components/Hero.jsx
+++ HooBank/src/components/Hero.jsx
@@ -11,11 +11,18 @@ export default function Hero() {
       >
         <div className="flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2">
           <img src={discount} alt="discount" className="w-[32px] h-[32px]" />
-          <p>
-            <span className="text-white">20%</span>
+          <p className={`${styles.paragraph} ml-2`}>
+            <span className="text-white">20% </span>
             Discount For <span className="text-white">1 Month </span>
             Account
           </p>
+        </div>
+        <div className="flex flex-row justify-between items-center w-full">
+          <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100px] leading-[75px]">
+            The Next <br className="sm:block hidden" />{" "}
+            <span className="text-gradient">Generation </span>
+            Payment Method.
+          </h1>
         </div>
       </div>
     </section>
