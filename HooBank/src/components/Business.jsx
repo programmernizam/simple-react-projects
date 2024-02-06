17d83c5ab6d14c1d6ae0d03f1a87cda0cd34b065
programmerNizam <mdnizam12109@gmail.com>
2024-02-06 20:57:35 +0600

    [Added Content] Business Section

--- HooBank/src/components/Business.jsx
+++ HooBank/src/components/Business.jsx
@@ -1,3 +1,15 @@
+// import { features } from "../constants";
+import styles, { layout } from "../style";
+// import Button from "./Button";
 export default function Business() {
-  return <div>Business</div>;
+  return (
+    <section id="features" className={layout.section}>
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>
+          You do the business, <br className="sm:block hidden" /> well handle
+          the money.
+        </h2>
+      </div>
+    </section>
+  );
 }
