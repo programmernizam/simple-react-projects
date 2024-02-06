d2a83c7851fb9c72781384ed44a6969ff0c5ad1e
programmerNizam <mdnizam12109@gmail.com>
2024-02-06 21:01:49 +0600

    [Added Content] Business Section

--- HooBank/src/components/Business.jsx
+++ HooBank/src/components/Business.jsx
@@ -1,14 +1,20 @@
 // import { features } from "../constants";
 import styles, { layout } from "../style";
-// import Button from "./Button";
+import Button from "./Button";
 export default function Business() {
   return (
     <section id="features" className={layout.section}>
       <div className={layout.sectionInfo}>
         <h2 className={styles.heading2}>
-          You do the business, <br className="sm:block hidden" /> well handle
-          the money.
+          You do the business, <br className="sm:block hidden" /> we&rsquo;ll
+          handle the money.
         </h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+          With the right credit card, you can improve your financial life by
+          building credit, earning rewards and saving money. But with hundreds
+          of credit cards on the market.
+        </p>
+        <Button styles="mt-10" />
       </div>
     </section>
   );
