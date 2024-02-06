0e667d1c89d24ff03d291e94ef3c62a3242f78a2
programmerNizam <mdnizam12109@gmail.com>
2024-02-06 10:58:01 +0600

    [Added & Styles] Stats Section Contents

--- HooBank/src/components/Stats.jsx
+++ HooBank/src/components/Stats.jsx
@@ -1,5 +1,14 @@
+import { stats } from "../constants";
+import styles from "../style";
 export default function Stats() {
   return (
-    <div>Stats</div>
-  )
+    <div className={`${styles.flexCenter} flex-row flex-wrap sm:mb-20 mb-6`}>
+      {stats.map((stat) => (
+        <div key={stat.id} className="">
+          <h4>{stat.value}</h4>
+          <p>{stat.title}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
