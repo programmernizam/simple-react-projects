a5e229b2d6f1b7d5a5a5925fa685edec8803c164
programmerNizam <mdnizam12109@gmail.com>
2024-02-07 13:46:08 +0600

    [Building] Card Deal Left Section

--- HooBank/src/components/CardDeal.jsx
+++ HooBank/src/components/CardDeal.jsx
@@ -1,5 +1,14 @@
+import { card } from "../assets";
+import styles, { layout } from "../style";
 export default function CardDeal() {
   return (
-    <div>CardDeal</div>
-  )
+    <section className={layout.section}>
+      <div className={layout.sectionInfo}>
+        <h2>
+          Find a better card deal <br className="sm:block hidden" /> in few easy
+          step.
+        </h2>
+      </div>
+    </section>
+  );
 }
