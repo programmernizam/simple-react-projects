c70c7c25ea00b02fd9fbd4a834ea81ee716dd57c
programmerNizam <mdnizam12109@gmail.com>
2024-02-07 14:26:58 +0600

    [Building] CTA Left side

--- HooBank/src/components/CTA.jsx
+++ HooBank/src/components/CTA.jsx
@@ -1,5 +1,18 @@
+import Button from "./Button";
+
 export default function CTA() {
   return (
-    <div>CTA</div>
-  )
+    <section>
+      <div>
+        <h2>Let’s try our service now!</h2>
+        <p>
+          Everything you need to accept card payments and grow your business
+          anywhere on the planet.
+        </p>
+      </div>
+      <div>
+        <Button />
+      </div>
+    </section>
+  );
 }
