60073beaefde7fff75b3e3125c866f56ec571ca4
programmerNizam <mdnizam12109@gmail.com>
2024-02-07 11:14:00 +0600

    [Added] Billing Left side img

--- HooBank/src/components/Billing.jsx
+++ HooBank/src/components/Billing.jsx
@@ -1,3 +1,20 @@
+import { apple, bill, google } from "../assets";
+import styles, { layout } from "../style";
 export default function Billing() {
-  return <div>Billing</div>;
+  return (
+    <section id="product" className={layout.sectionImgReverse}>
+      <div className={layout.sectionImgReverse}>
+        <img
+          src={bill}
+          alt="billing"
+          className="w-[100%] h-[100%] relative z-[5]"
+        />
+        <div className="absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white_gradient"/>
+        <div className="absolute z-[0] -left-1/2 bottom-0 w-[50%] h-[50%] rounded-full pink__gradient"/>
+      </div>
+      <div className={layout.sectionInfo}>
+        <h2></h2>
+      </div>
+    </section>
+  );
 }
