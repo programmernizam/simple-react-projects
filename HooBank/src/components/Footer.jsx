ec13127bdd4c3d57dcdb4f5f1cd4ca226801e36c
programmerNizam <mdnizam12109@gmail.com>
2024-02-08 13:29:02 +0600

    [Completed] HooBank Home page v1

--- HooBank/src/components/Footer.jsx
+++ HooBank/src/components/Footer.jsx
@@ -1,5 +1,5 @@
 import { logo } from "../assets";
-import { footerLinks } from "../constants";
+import { footerLinks, socialMedia } from "../constants";
 import styles from "../style";
 export default function Footer() {
   const year = new Date().getFullYear();
@@ -41,10 +41,22 @@ export default function Footer() {
           ))}
         </div>
       </div>
-      <div>
+      <div className="w-full flex justify-between items-center md:flex-row flex-col pt-6 border-t-[1px] border-t-[#3FE45]">
         <p className="font-poppins font-normal text-center text-[18px] leading-[27px] text-white">
           {year} HooBank. All Rights Reserved.
         </p>
+        <div className="flex flex-row md:mt-0 mt-6">
+          {socialMedia.map((social, index) => (
+            <img
+              key={social.id}
+              src={social.icon}
+              alt={social.id}
+              className={`w-[21px] h-[21px] object-contain cursor-pointer ${
+                index !== socialMedia.length - 1 ? "mr-6" : "mr-0"
+              }`}
+            />
+          ))}
+        </div>
       </div>
     </footer>
   );
