43836b6399cc533b125279cca3a300dd92da96eb
programmerNizam <mdnizam12109@gmail.com>
2024-02-08 12:15:31 +0600

    [Building] Footer Section add logo content

--- HooBank/src/components/Footer.jsx
+++ HooBank/src/components/Footer.jsx
@@ -1,3 +1,13 @@
+import { logo } from "../assets";
+import styles from "../style";
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <footer className={`${styles.flexCenter} ${styles.paddingY} flex-col`}>
+      <div className={`${styles.flexStart} md:flex-row flex-col mb-8 w-full`}>
+        <div>
+          <img src={logo} alt="hooBank" />
+        </div>
+      </div>
+    </footer>
+  );
 }
