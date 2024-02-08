d84942e21dfb62b150a2e3052f221fc815253b27
programmerNizam <mdnizam12109@gmail.com>
2024-02-08 12:40:59 +0600

    [Building] Footer Section footerlink data update

--- HooBank/src/components/Footer.jsx
+++ HooBank/src/components/Footer.jsx
@@ -1,11 +1,30 @@
 import { logo } from "../assets";
+import { footerLinks } from "../constants";
 import styles from "../style";
 export default function Footer() {
   return (
     <footer className={`${styles.flexCenter} ${styles.paddingY} flex-col`}>
       <div className={`${styles.flexStart} md:flex-row flex-col mb-8 w-full`}>
-        <div>
-          <img src={logo} alt="hooBank" />
+        <div className="flex-1 flex flex-col justify-start mr-10">
+          <img
+            src={logo}
+            alt="hooBank"
+            className="w-[266px] h-[72px] object-contain"
+          />
+          <p className={`${styles.paragraph} mt-4 max-w-[310px]`}>
+            A new way to make payment easy, reliable and secure.
+          </p>
+        </div>
+        <div className="flex-[1.5] w-full flex flex-row justify-between flex-wrap md:mt-0 mt-10">
+          {footerLinks.map((footerLinks) => (
+            <div
+              key={footerLinks.key}
+              className="flex flex-col ss:my-0 my-4 min-w-[150px]"
+            >
+              <h4>{footerLinks.title}</h4>
+              <ul></ul>
+            </div>
+          ))}
         </div>
       </div>
     </footer>
